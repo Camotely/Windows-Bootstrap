bb22601e7d81fbff9e6ed80522fc94cd7bf3c1db
Camotely <>
2021-02-15 15:23:35 -0500

    Test

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -9,25 +9,37 @@ Disable-UAC
 $ConfirmPreference = "None"
 
 #$scriptUri = "https://raw.githubusercontent.com/Camotely/Windows-Boostrap/main/scripts"
-#function executeScript {
-#    Param ([string]$script)
-#    Write-Host "Running $script..."
-#        Invoke-Expression ((New-Object net.webclient).DownloadString("$scriptUri/$script"))
-#}
+
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$strpos = $helperUri.LastIndexOf("/")
+$helperUri = $helperUri.Substring(0, $strpos)
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
 
 Write-Host "File Explorer" -ForegroundColor "Yellow"
-#executeScript "FileExplorer.ps1";
+executeScript "FileExplorer.ps1";
 Write-Host "Debloat" -ForegroundColor "Yellow"
-#executeScript "Debloat.ps1";
+executeScript "Debloat.ps1";
 Write-Host "Programs" -ForegroundColor "Yellow"
-#executeScript "Programs.ps1";
+executeScript "Programs.ps1";
 
 Write-Host "Hyper V" -ForegroundColor "Yellow"
-#executeScript "Hyper-V.ps1";
+executeScript "Hyper-V.ps1";
 Write-Host "Refreshing" -ForegroundColor "Yellow"
 RefreshEnv
 Write-Host "WSL" -ForegroundColor "Yellow"
-#executeScript "WSL.ps1";
+executeScript "WSL.ps1";
 
 Write-Host "UAC" -ForegroundColor "Yellow"
 # Re-enabling UAC
