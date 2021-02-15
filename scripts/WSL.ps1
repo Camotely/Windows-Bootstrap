fbfb82d70107e6a2f44450964f3d7d18aba84148
Camotely <>
2021-02-15 16:47:31 -0500

    Added comments

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -1,8 +1,10 @@
+# WSL2 installation
 choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
 RefreshEnv
 choco install -y wsl2
 RefreshEnv
 choco install -y wsl-ubuntu-2004
 
+# Update and upgrade WSL2
 wsl -u root apt update
 wsl -u root apt upgrade -y
